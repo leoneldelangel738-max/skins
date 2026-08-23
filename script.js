1886affb26609c48b3f82095581136cd2ed67c75
leoneldelangel738-max <leoneldelangel738@gmail.com>
2026-08-23 15:25:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,111 @@
-console.log("Skin Gallery cargada correctamente");
+const defaultSkins = [
+    {
+        name: "Default 01",
+        image: "images/sora/default/01.png"
+    },
+    {
+        name: "Default 02",
+        image: "images/sora/default/02.png"
+    }
+];
+
+
+const customSkins = [
+    {
+        name: "Skin 01",
+        image: "images/sora/custom/01.png"
+    },
+    {
+        name: "Skin 02",
+        image: "images/sora/custom/02.png"
+    },
+    {
+        name: "Skin 03",
+        image: "images/sora/custom/03.png"
+    }
+];
+
+
+function mostrarSkins(lista, contenedor) {
+
+    lista.forEach(skin => {
+
+        const card = document.createElement("div");
+
+        card.classList.add("skin-card");
+
+
+        const nombre = document.createElement("div");
+
+        nombre.classList.add("skin-name");
+
+        nombre.textContent = skin.name;
+
+
+        const imagen = document.createElement("img");
+
+        imagen.src = skin.image;
+
+        imagen.alt = skin.name;
+
+
+        card.appendChild(nombre);
+
+        card.appendChild(imagen);
+
+
+        card.addEventListener("click", function () {
+
+            mostrarPreview(
+                skin.image,
+                skin.name
+            );
+
+        });
+
+
+        contenedor.appendChild(card);
+
+    });
+
+}
+
+
+function mostrarPreview(imagen, nombre) {
+
+    const previewImage =
+        document.getElementById("previewImage");
+
+    const previewText =
+        document.getElementById("previewText");
+
+
+    previewImage.src = imagen;
+
+    previewImage.alt = nombre;
+
+    previewImage.style.display = "block";
+
+    previewText.style.display = "none";
+
+}
+
+
+const defaultContainer =
+    document.getElementById("defaultSkins");
+
+
+const customContainer =
+    document.getElementById("customSkins");
+
+
+mostrarSkins(
+    defaultSkins,
+    defaultContainer
+);
+
+
+mostrarSkins(
+    customSkins,
+    customContainer
+);
